94d6e90fe7ce977c8028b4ecfdb31abdad6cf91a
libertadcc <libertadchapinalcervantes@gmail.com>
2019-06-28 20:46:29 +0200

    Add classes to edit scss

--- src/Components/Edit/Edit.js
+++ src/Components/Edit/Edit.js
@@ -3,58 +3,69 @@ import { Link } from 'react-router-dom';
 
 function Edit({updateMood, updateDate}) {
   return (
-  <div>
-    <React.Fragment>
-    <p>Edit</p>
-      <form>
-        <div>
-          <label htmlFor="date">
-            Fecha
+  <div className="edit__page">
+    <h1 className="edit__title">¿Cómo te ha ido el día?</h1>
+    <form className="edit__form">
+      <div className="form__date">
+        <label 
+        htmlFor="date"
+        className="form__date--title">
+          Fecha
+        </label>
+        <input  
+        className="form__date--input"
+        type="date" 
+        required
+        onChange={updateDate}/>
+      </div>
+      <div className="form__mood">
+        <div className="form__mood--happy">
+          <label 
+          htmlFor="happy"
+          className="happy__title">
+            <input 
+            className="happy__input"
+            type="radio" 
+            name="state" 
+            defaultValue="happy" 
+            onClick={updateMood}/> 
+            :)
           </label>
-          <input 
-          type="date" 
-          required
-          onChange={updateDate}/>
         </div>
-        <div>
-          <div>
-            <label htmlFor="happy">
-              <input 
-              type="radio" 
-              name="state" 
-              defaultValue="happy" 
-              onClick={updateMood}/> 
-              :)
-            </label>
-          </div>
-          <div>
-            <label htmlFor="sad">
-              <input 
-              type="radio" 
-              name="state" 
-              defaultValue="sad" 
-              onClick={updateMood}/> 
-              :( 
-            </label>
-          </div>
+        <div className="form__mood--sad">
+          <label 
+          htmlFor="sad"
+          className="sad__title">
+            <input 
+            className="sad__input"
+            type="radio" 
+            name="state" 
+            defaultValue="sad" 
+            onClick={updateMood}/> 
+            :( 
+          </label>
         </div>
-        <div>
-          <textarea 
-          name="opinion" 
-          placeholder="¿Por qué es un buen día?"/>
-        </div>
-        <div>
-          <Link to="/">
-            <input type="submit" />
-          </Link>
-          <Link to="/">
-            <button>
-              Cancelar
-            </button>
-          </Link>
-        </div>
-      </form>
-    </React.Fragment>
+      </div>
+      <div className="form__opinion"> 
+        <textarea 
+        name="opinion" 
+        placeholder="¿Por qué es un buen día?"/>
+      </div>
+      <div className="edit__buttons">
+        <Link
+        className="edit__save"
+        to="/">
+          <input type="submit" />
+        </Link>
+        <Link 
+        className="edit__cancel"
+        to="/">
+          <button>
+            Cancelar
+          </button>
+        </Link>
+      </div>
+    </form>
    </div>
   );
 }
