810574e3d6e3da0545831cced01cc533cabb04c3
libertadcc <libertadchapinalcervantes@gmail.com>
2019-06-28 12:46:25 +0200

    Add navigation between components using link

--- src/Components/Edit/Edit.js
+++ src/Components/Edit/Edit.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Edit() {
-  return <p>Edit</p>
+  return (
+  <div>
+    <p>Edit</p>
+    <Link to="/">
+      <button>
+        Guardar
+      </button>
+    </Link>
+    <Link to="/">
+      <button>
+        Cancelar
+      </button>
+    </Link>
+   </div>
+  );
 }
 
 export default Edit;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return <p>Home</p>
+  return (
+    <div>
+      <Link to="/edit">
+        <button>
+          +
+        </button>
+      </Link>
+      <p>Home</p>
+    </div>
+  );
 }
 
 export default Home;
